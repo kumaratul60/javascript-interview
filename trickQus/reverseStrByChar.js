17b02c18b760034a9289159f95c3096bcebc7fb0
Atul <atulreso1@gmail.com>
2022-11-01 16:08:25 +0530

    flip-str by world

---
+++ trickQus/reverseStrByChar.js
@@ -0,0 +1,8 @@
+function flip(str) {
+  let temp = "";
+  for (let i = 0; i < str.length; i += 2)
+    temp += (i + 1 < str.length ? str[i + 1] : "") + str[i];
+  return temp;
+}
+const res = flip("world");
+console.log(res); // owlrd
