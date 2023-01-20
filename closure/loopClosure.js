1ae8eab0ef2e180cc45ebb17db05255c4e264348
Atul <atulreso1@gmail.com>
2023-01-20 14:34:45 +0530

    Add: closure loop imp ques

--- closure/loopClosure.js
+++ closure/loopClosure.js
@@ -7,6 +7,69 @@
 // }
 //  foo();
 
-for (var i = 0; i < 5; i++) {
-  setTimeout(() => console.log(i), 0);
+// for (var i = 0; i < 5; i++) {
+//   // setTimeout(() => console.log(i), 1000*i);
+//   setTimeout(() => console.log(i), 1000+i);
+// }
+
+function ex() {
+  for (var i = 1; i <= 5; i++) {
+    setTimeout((function (k) {
+      return (() => { console.log(k) })
+    })(i), 1000 * i);
+  }
+
 }
+// ex()
+
+
+function ex1() {
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(((k) => {
+      return (() => { console.log(k) })
+    })(i), 1000 * i);
+  }
+
+}
+// ex1()
+
+
+function ex2() {
+  for (var i = 1; i <= 5; i++) {
+    function inner(j) {
+      setTimeout(() => {
+        console.log(j)
+      }, 1000 * i);
+    } inner(i)
+  }
+
+}
+// ex2()
+
+
+function ex3() {
+  for (var i = 0; i <= 5; i++) {
+    const inner = (j) => {
+      setTimeout(() => {
+        console.log(j)
+      }, 1000 * i);
+
+    }
+    inner(i)
+
+  }
+
+}
+// ex3()
+
+function ex4() {
+  for (var i = 1; i <= 5; i++) {
+    (function (j) {
+      setTimeout(() => {
+        console.log(j)
+      }, 1000 * i);
+    })(i)
+  }
+
+}
+ex4()
