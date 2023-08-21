0b3ad4eba0c26f8261571ab9b2a11da825c653cf
Atul <atulreso1@gmail.com>
2023-08-21 20:36:11 +0530

    boom

--- closure/printDecrementCounter.js
+++ closure/printDecrementCounter.js
@@ -6,3 +6,15 @@ for (let i = 10; i >= 0; i--) {
         console.log(i);
     }, (10 - i) * 800)
 }
+
+// IFFY ES5 way
+
+(function () {
+    for (var i = 10; i >= 0; i--) {
+      (function (count) {
+        setTimeout(function () {
+          console.log(count);
+        },(10-i)* 1000);
+      })(i);
+    }
+  })();
