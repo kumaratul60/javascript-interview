e69b2f8a5e95d3d97a2f45aa0dff982c093bc7de
Atul <atulreso1@gmail.com>
2023-08-27 04:48:11 +0530

    Add forEach polifill

--- Pollyfil/forEach.js
+++ Pollyfil/forEach.js
@@ -4,13 +4,40 @@ Array.prototype.MyForEach = function (callback) {
   }
 };
 
-var logicAlbums = [
+const logicAlbums = [
   "Bobby",
   "The Incredible True Story",
   "supermarket",
   "underPressure",
 ];
 
-logicAlbums.MyForEach((word) => {
-  console.log(word);
+// Advance forEach implementation
+
+Array.prototype.AdvForEach = function (callback, currContext) {
+  if (typeof callback !== "function") {
+    throw new Error("AdvForEach is not a function");
+  }
+  const length = this.length;
+
+  //using for loop
+
+  // for (let i = 0; i < length; i++) {
+  //   if (this.hasOwnProperty(i)) {
+  //     callback.call(currContext, this[i], i, this);
+  //   }
+  // }
+
+  // using while loop
+  let i = 0;
+  while (i < length) {
+    // this.hasOwnProperty(i) -> to avoid extra property if any one added in prototype
+    if (this.hasOwnProperty(i)) {
+      callback.call(currContext, this[i], i, this);
+    }
+    i++;
+  }
+};
+
+logicAlbums.AdvForEach((word) => {
+  console.log({ word });
 });
