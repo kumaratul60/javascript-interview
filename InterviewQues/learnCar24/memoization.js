5a9c11ad30b9f9df7cd4a0eabbc4f86aa9867261
Atul <atulreso1@gmail.com>
2023-09-22 11:21:54 +0530

    add memo for square

--- InterviewQues/learnCar24/memoization.js
+++ InterviewQues/learnCar24/memoization.js
@@ -3,9 +3,27 @@
 //  todo: search that ques ans
 
 const square = (a, b) => {
-  for (let i = 1; i <= 1000000; i++) {}
+  for (let i = 1; i <= 100000000; i++) {}
   return a * b;
 };
+
+const memo = (fn) => {
+  let cache = {};
+  return function (a, b) {
+    const args = `${a},${b}`;
+    if (cache[args]) {
+      console.log("from cache");
+      return cache[args];
+    } else {
+      console.log("from calculation");
+      const result = fn(a, b);
+      cache[args] = result;
+      return result;
+    }
+  };
+};
+
+const res1 = memo(square);
 
 console.time("first call");
 console.log(square(9467, 7649));
@@ -14,3 +32,6 @@ console.timeEnd("first call");
 console.time("second call");
 console.log(square(9467, 7649));
 console.timeEnd("second call");
+
+console.log(res1(9467, 7649));
+console.log(res1(9467, 7649));
--- InterviewQues/learnCar24/oouput.js
+++ InterviewQues/learnCar24/oouput.js
@@ -9,4 +9,3 @@ console.log("b");
 // b
 // pro
 // set
- 
