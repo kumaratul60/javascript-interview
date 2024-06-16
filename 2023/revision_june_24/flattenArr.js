96bd9ba8a71cea6097a8cef06b2740d1be9d968c
Atul <atulreso1@gmail.com>
2024-06-16 15:00:26 +0530

    flate arr

--- 2023/revision_june_24/flattenArr.js
+++ 2023/revision_june_24/flattenArr.js
@@ -1,64 +1,81 @@
-const a = [1, 2, 3, [4, [5, 6]], [{t:"l"}], [7], 8, [9, 10]];
+const a = [1, 2, 3, [4, [5, 6]], [{ t: "l" }], [7], 8, [9, 10]];
+const arr = [[1, [2]], [3, [4, 5]], [6, 7], 8];
+
+// optimise way o(1) space
+function flattenInPlace(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    while (Array.isArray(arr[i])) {
+      // Replace the current array element with its elements
+      arr.splice(i, 1, ...arr[i]);
+    }
+  }
+  return arr;
+}
+
+const flatTest = flattenInPlace(a, 2);
+
+console.log({ flatTest });
 
 // best way
 
-function flattenBest(arr,flattenArr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])) {
-            flattenBest(arr[i], flattenArr)
-        } else {
-            flattenArr.push(arr[i]);
-        }
+function flattenBest(arr, flattenArr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattenBest(arr[i], flattenArr);
+    } else {
+      flattenArr.push(arr[i]);
     }
-    return flattenArr
+  }
+  return flattenArr;
 }
 
 //Using while loop with a recursive helper function
 function flattenArr(nested) {
-    const flat = [];
+  const flat = [];
 
-    function handleFlat(array) {
-        let counter = 0;
-        while (counter < array.length) {
-            const val = array[counter];
-            if (Array.isArray(val)) {
-                handleFlat(val);
-            } else {
-                flat.push(val);
-            }
-            counter++;
-        }
+  function handleFlat(array) {
+    let counter = 0;
+    while (counter < array.length) {
+      const val = array[counter];
+      if (Array.isArray(val)) {
+        handleFlat(val);
+      } else {
+        flat.push(val);
+      }
+      counter++;
     }
+  }
 
-    handleFlat(nested);
-    return flat;
+  handleFlat(nested);
+  return flat;
 }
 
 // to enable deep level flatten use recursion with reduce and concat
 function flatDeep(arr, d = 1) {
-    return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val), [])
-        : arr.slice();
-};
+  return d > 0
+    ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val), [])
+    : arr.slice();
+}
 
 // using Stack
 // non recursive flatten deep using a stack
 // note that depth control is hard/inefficient as we will need to tag EACH value with its own depth
 // also possible w/o reversing on shift/unshift, but array OPs on the end tends to be faster
 function stackFlatten(input) {
-    const stack = [...input];
-    const res = [];
-    while (stack.length) {
-        // pop value from stack
-        const next = stack.pop();
-        if (Array.isArray(next)) {
-            // push back array items, won't modify the original input
-            stack.push(...next);
-        } else {
-            res.push(next);
-        }
+  const stack = [...input];
+  const res = [];
+  while (stack.length) {
+    // pop value from stack
+    const next = stack.pop();
+    if (Array.isArray(next)) {
+      // push back array items, won't modify the original input
+      stack.push(...next);
+    } else {
+      res.push(next);
     }
-    // reverse to restore input order
-    return res.reverse();
+  }
+  // reverse to restore input order
+  return res.reverse();
 }
 
 // const output = flattenArr(a);
@@ -66,5 +83,5 @@ function stackFlatten(input) {
 // const output = flatDeep(a, Infinity);
 // const output = stackFlatten(a);
 
-const output = flattenBest(a,[])
-console.log(output);
+// const output = flattenBest(a,[])
+// console.log(output);
