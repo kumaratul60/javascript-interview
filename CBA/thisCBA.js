f4d0bf84670b1339c82a009269ffe1bcba3080f9
Atul <atulreso1@gmail.com>
2025-03-23 11:22:51 +0530

    this CBA

--- CBA/thisCBA.js
+++ CBA/thisCBA.js
@@ -68,5 +68,100 @@ console.log(food.getDesc());
  * implicit binding: whenever you calling the object name dot the method, you have to check what is that particular method is about:
  if the method is a standard javascript function and non arrow function and if that function has the this keyword, the this keyword is bound to the object on which you are calling the function or the method.
 
- if that function happen to be an arrow function whether it is inside an object or outside an object wherever it is, it all depends where the arrow function is lexically placed or defined in your code, check the parent scope of the place where arrow function is defined because arrow function doesn't have its own this so the this always refer for an arrow function to the parent scope of the scope where arrow function is defined. 
+ if that function happen to be an arrow function whether it is inside an object or outside an object wherever it is, it all depends where the arrow function is lexically placed or defined in your code, check the parent scope of the place where arrow function is defined because arrow function doesn't have its own this so the this always refer for an arrow function to the parent scope of the scope where arrow function is defined.
  */
+
+/* Explicit binding: If you want to refer from 1 execution context to another execution context, and want to bind that two context.
+
+Explicit binding achieved by using call, apply and bind method.
+*/
+
+// call:
+
+function callGreeting() {
+  console.log(`Hi ${this.name}`);
+}
+const callUser = {
+  name: "Atul",
+};
+callGreeting.call(callUser);
+
+const callParamsLikes = function (h1, h2) {
+  console.log(`Hi ${this.name} you like ${h1} and ${h2}`);
+};
+const callParamsUser = {
+  name: "Atul",
+};
+callParamsLikes.call(callParamsUser, "Challenge", "Coding");
+
+const likesApply = ["Code", "YT"];
+callParamsLikes.apply(callParamsUser, likesApply);
+
+//bind
+const bindGreeting = function (p1, p2) {
+  console.log(`Hi ${this.name} you like ${p1} and ${p2}`);
+};
+
+const offBind = {
+  name: "Atul",
+};
+
+const bindRes = bindGreeting.bind(offBind, "CrossFit", "Snakes");
+bindRes();
+
+// new => to create different instance of object
+
+const Cartoon = function name(name, animal) {
+  this.name = name;
+  this.animal = animal;
+  //     this.log = function () {
+  //         console.log(this.name, this.animal);
+  // }
+
+  this.log = () => {
+    console.log(this.name, this.animal);
+  };
+};
+
+const tom = new Cartoon("Tom", "Cat");
+const jerry = new Cartoon("Jerry", "Mouse");
+tom.log();
+jerry.log();
+
+const myLog = {
+  name: "Kumar",
+  greet: function () {
+    // function inner() {
+    //   console.log(`Hi ${this.name}`);
+    // }
+
+    const inner = () => {
+      console.log(`Hi ${this.name}`);
+    };
+    inner();
+  },
+};
+
+myLog.greet();
+
+const obj1 = {
+  name: "john",
+  greet: function () {
+    console.log(`Hi ${this.name}`);
+  },
+};
+
+//  obj1.greet(); // Hi john
+
+// const res = obj1.greet();
+// console.log(res)// undefine
+
+///// error so we use explicit binding
+// const res = obj1.greet;
+// res();
+
+const res = obj1.greet;
+res.call(obj1);
+
+// const res = obj1.greet.bind(obj1);
+// const res = obj1.greet.call(obj1); // error
