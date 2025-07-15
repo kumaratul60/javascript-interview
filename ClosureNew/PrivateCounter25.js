28d0444b5aef1cf5fc6af7fa195fd59d57a00fa5
Atul <atulreso1@gmail.com>
2025-07-15 20:38:42 +0530

    private counter set

---
+++ ClosureNew/PrivateCounter25.js
@@ -0,0 +1,25 @@
+function createCounter() {
+  let count = 0;
+  return {
+    increment: function () {
+      count++;
+      console.log(`increment by ${count}`);
+    },
+    decrement: function () {
+      count--;
+      console.log(`decrement by ${count}`);
+    },
+    reset: function () {
+      count = 0;
+      console.log(`counter reset by  ${count}`);
+    },
+  };
+}
+
+const counter = createCounter()
+counter.increment()
+counter.increment();
+counter.increment();
+counter.decrement();
+counter.decrement();
+counter.reset();
