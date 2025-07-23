0e25764d777a6bc3fb70145b6f1f5be082121ace
Atul <atulreso1@gmail.com>
2025-07-23 18:51:37 +0530

    feat(js): DOM 1

---
+++ DOM/index.js
@@ -0,0 +1,19 @@
+console.log("test dom");
+
+// Dom:
+// Dom Types:
+// 1. Document
+// Document is a root node in the DOM tree, It exist on top of HTML. The document represent entire tree.
+console.log(document, document.head, document.title, document.body); // complete web page under document
+console.log(document.URL); // current page url
+
+// 2. Node: Any item in dom tree is called as node. i.e: Element Node, Text Node, Attribute Node.
+// 3. Element: A specific type of node that represents HTML tags/elements like <p>,<div>, etc
+//  4. NodeList: An array of Nodes
+// 5. Attr: It represents the attributes of a node.
+        // <img src = "/path" alt="some image"/> // so this img has 2 attribute
+// 6. NameNodeMap: A collection of Attr.
+
+
+// Accessing DOM:
+
