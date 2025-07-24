55313e988237adc0dc4f0e8283fc9384106b5e18
Atul <atulreso1@gmail.com>
2025-07-24 11:53:05 +0530

    DOM: p1

--- DOM/index.js
+++ DOM/index.js
@@ -11,9 +11,54 @@ console.log(document.URL); // current page url
 // 3. Element: A specific type of node that represents HTML tags/elements like <p>,<div>, etc
 //  4. NodeList: An array of Nodes
 // 5. Attr: It represents the attributes of a node.
-        // <img src = "/path" alt="some image"/> // so this img has 2 attribute
+// <img src = "/path" alt="some image"/> // so this img has 2 attribute
 // 6. NameNodeMap: A collection of Attr.
 
-
 // Accessing DOM:
+// id vs class: Id will be unique for across the web pages but class can be repeated.
 
+// by Id
+let titleElem = document.getElementById("heading");
+console.log({ titleElem }, titleElem);
+
+// by class
+let titleClassElem = document.getElementsByClassName("info");
+console.log(titleClassElem); // HTMLCollection(2): [p.info, p.info]
+// it is an array-like, that look like array but not an array because it's prototype is a HTMLCollection and when expand that prototype collection it shows as prototype as Object not Array.
+
+// How to iterate over the titleClassElem
+// 1. console.log(titleClassElem[0], titleClassElem[1]);
+// 2.
+[...titleClassElem].forEach((elem) => {
+  console.log(elem);
+});
+
+// By Tag name
+const pTagElem = document.getElementsByTagName("p");
+console.log(pTagElem); // HTMLCollection(2):
+
+// Selectors: Query Selector and Query Selector All
+// Query Selector - querySelector(): it is responsible for giving you the first matching element node with the selector that we passing as argument in to it.
+
+// Selectors: In CSS the selectors are the pattern to reach and select the elements with the state of css rules.
+// There are different selectors, combinator, separators are available in css using which you can select a particular element.
+
+const para = document.querySelector("p");
+const paraInfo = document.querySelector("p.info");
+console.log(paraInfo, "::para"); // first p tag shows
+
+// querySelectorAll()
+const paras = document.querySelectorAll("p.info");
+console.log(paras, "::paras"); // NodeList(2)
+
+// if you want to use id with querySelector use with pound sign(#), it'll similarly work as getElementById
+const h1All = document.querySelectorAll("heading"); //NodeList []
+const h1All1 = document.querySelectorAll("#heading"); //NodeList [h1#heading]
+const h1Al2 = document.querySelector("heading"); // null
+const h1Al3 = document.querySelector("#heading"); //<h1 id="heading">"Learning Dom"</h1>
+console.log(h1All, h1All1, h1Al2, h1Al3, "::hall");
+
+// document.getElementsByName(elementName);
+// document.getElementsByTagNameNS(namespaceURI, localName);
+// document.getRootNode();
+// document.getSelection()
