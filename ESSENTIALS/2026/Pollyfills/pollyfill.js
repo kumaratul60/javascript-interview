e8744548273a83389667ad9c9834434ebee12650
Atul Kumar Awasthi <atulreso1@gmail.com>
2026-06-20 18:04:02 +0530

    feat: add initial pollyfill.js with core functionality descriptions

---
+++ ESSENTIALS/2026/Pollyfills/pollyfill.js
@@ -0,0 +1,10 @@
+/***
+ * Promise.all/race/any/allsettled
+ * call/bind/apply/map/filter/reduce
+ * setTimeout/clearTimeout/clearAllTimeout/setInterval
+ * Promise from scratch, new promise, batching in promise, series vs parallel execution
+ * map,weekMap, Set weekSet
+ * useEffect pollyfill
+ * webworker
+ *
+ */
